b156d8d969ae81d9067aa2b092fe5b041298c818
0xskaper <es.rajatyadav@gmail.com>
2025-07-26 16:56:05 -0400

    feat: Exercise 1.6

--- qnx-1/qnx-1.c
+++ qnx-1/qnx-1.c
@@ -99,20 +99,88 @@
 //
 // 1.5
 //
-int main(int argc, char **argv, char **envp) {
-  pid_t pid;
-  int child_status;
+// int main(int argc, char **argv, char **envp) {
+//   pid_t pid;
+//   int child_status;
+//
+//   pid = fork();
+//   if (pid == -1) {
+//     perror("FORK()\n");
+//     exit(EXIT_FAILURE);
+//   } else if (pid > 0) {
+//     printf("I AM CHILD AND NOW I'LL EXEC\n");
+//     execl("/proc/boot/ls", "ls", "-l", "-a", NULL);
+//   } else {
+//     printf("I AM PARENT AND I'LL WAIT\n");
+//     sleep(5);
+//     pid = wait(&child_status);
+//   }
+// }
+//
+// 1.6
+//
+#define NUM_THREADS 5
 
-  pid = fork();
-  if (pid == -1) {
-    perror("FORK()\n");
-    exit(EXIT_FAILURE);
-  } else if (pid > 0) {
-    printf("I AM CHILD AND NOW I'LL EXEC\n");
-    execl("/proc/boot/ls", "ls", "-l", "-a", NULL);
-  } else {
-    printf("I AM PARENT AND I'LL WAIT\n");
-    sleep(5);
-    pid = wait(&child_status);
+long long factorial_results[NUM_THREADS];
+
+pthread_mutex_t result_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct {
+  int thread_id;
+  int number;
+} thread_data_t;
+
+long long factorial(int n) {
+  if (n <= 1)
+    return 1;
+  long long result = 1;
+  for (int i = 2; i <= n; i++) {
+    result *= i;
   }
+
+  return result;
+}
+
+void *do_fact_thread(void *arg) {
+  thread_data_t *data = (thread_data_t *)arg;
+
+  printf("Thread: %d -> WORKING ON -> %d\n", data->thread_id, data->number);
+
+  long long result = factorial(data->number);
+
+  pthread_mutex_lock(&result_mutex);
+  factorial_results[data->thread_id] = result;
+  printf("THREAD %d: %d! = %lld\n", data->thread_id, data->number, result);
+  pthread_mutex_unlock(&result_mutex);
+
+  return NULL;
+}
+
+int main(int argc, char **argv, char **envp) {
+  pthread_t threads[NUM_THREADS];
+  thread_data_t thread_data[NUM_THREADS];
+
+  for (int i = 0; i < NUM_THREADS; i++) {
+    thread_data[i].thread_id = i;
+    thread_data[i].number = i + 1;
+
+    if (pthread_create(&threads[i], NULL, do_fact_thread, &thread_data[i]) !=
+        0) {
+      perror("pthread_create_failed");
+      exit(1);
+    }
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  printf("\nResult from shared Array\n");
+  for (int i = 0; i < NUM_THREADS; i++) {
+    printf("ARRAY %d: %d! = %lld\n", i, i + 1, factorial_results[i]);
+  }
+
+  pthread_mutex_destroy(&result_mutex);
+
+  return 0;
 }
